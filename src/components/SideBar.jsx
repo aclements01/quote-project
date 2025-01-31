8948762d4bc26c29eb917792a172e9bac5ae6612
aclements01 <aclements@senmc.edu>
2025-01-30 18:42:59 -0700

    working on styling

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,8 +5,11 @@ const SideBar = () => {
         <h1>Depressing Quote Array Project</h1>
         <h2>
           I kept an annotated database of quotes from books I read in graduate
-          school. Many of the sources are referencing memory, death, poetry,
-          esoteric thought, etc...
+          school.
+          <p>
+            Many of the sources are referencing memory, death, poetry, esoteric
+            thought, etc...
+          </p>
         </h2>
       </div>
     </div>
